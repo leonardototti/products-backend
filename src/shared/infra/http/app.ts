579e4e9af13988dcb5ede136d0a93cf88ae1f181
leonardo.totti <leonardo.totti@edu.unifil.br>
2024-11-25 13:30:59 -0300

    feat: :sparkles: Adiciona rota de arquivos de upload

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,11 +8,14 @@ import routes from "./routes";
 
 import "../../container";
 import errorHandling from "./middleware/errorHandling";
+import { resolve } from "path";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/files", express.static(resolve(process.env?.DISK_PATH || "tmp")));
 
 app.use(routes);
 app.use(errorHandling);
